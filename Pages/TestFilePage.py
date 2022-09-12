99ef9c0499b9a1af67d358c3c1fd666579f92d38
Mohit Jangra <mohit.jangra@360degreecloud.in>
2022-09-12 11:31:21 +0530

    Add Test Demo File By Mohit

---
+++ Pages/TestFilePage.py
@@ -0,0 +1,42 @@
+import time
+from telnetlib import EC
+from selenium.webdriver.common.by import By
+
+
+class TestFilePage:
+    logo_by_xpath = "//img[@class='brand-image-xl logo-xl']"
+    searchbox_by_id = "search-box"
+
+    def __init__(self, driver):
+        self.driver = driver
+
+    def i_should_see_logo(self):
+        logo = self.driver.find_element(By.XPATH, self.logo_by_xpath).is_displayed()
+        if logo == True:
+            assert True
+        else:
+            assert False
+
+    def i_should_see_search_box(self):
+        searchbox = self.driver.find_element(By.ID, self.searchbox_by_id).is_displayed()
+        if searchbox == True:
+            assert True
+        else:
+            assert False
+
+    def i_should_see_all_side_menu_item_is_visible(self):
+        menuitem = "//ul[@class='nav nav-pills nav-sidebar flex-column nav-legacy']//a[@href='#' ]//p"
+        getValue = self.driver.find_elements(By.XPATH, menuitem)
+        listitem = []
+        for all_item in getValue:
+            item_text = all_item.text
+            listitem.append(item_text)
+        print(listitem)
+        remove_blank_value = list(filter(None, listitem))
+        print(remove_blank_value)
+        for index_click in remove_blank_value:
+            item_click_by_xpath = f"//ul[@class='nav nav-pills nav-sidebar flex-column nav-legacy']//a[@href='#' and @class='nav-link']//p[contains(text(), '{index_click}')]"
+            self.driver.find_element(By.XPATH, item_click_by_xpath).click()
+            time.sleep(2)
+
+
---
+++ features/Test_File.feature
@@ -0,0 +1,19 @@
+# Created by Mohit at 12-09-2022
+Feature: SideBar
+  # Enter feature description here
+  Background:
+    Given Open Browser
+    And User On Login Page
+    And User login as admin
+
+    @SB_01
+  Scenario: Verify Side Bar Logo Is Visible
+    Given I should see logo
+
+    @SB_02
+  Scenario: Verify Side Bar Search Box Is Visible
+    Given I should see search box
+
+     @SB_03
+  Scenario: Verify side menu bar item is visible as main header
+       Given I should see all side menu item is visible
