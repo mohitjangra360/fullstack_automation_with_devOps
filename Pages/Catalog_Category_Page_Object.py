e496c4aaa970b44225f63e19371dff54b4366251
Mohit Jangra <mohit.jangra@360degreecloud.in>
2022-09-29 09:50:22 +0530

    add test case by rupali

--- Pages/Catalog_Category_Page_Object.py
+++ Pages/Catalog_Category_Page_Object.py
@@ -232,3 +232,187 @@ class Catalog_Category:
             self.driver.find_element(By.XPATH, "//div[@id='pnlPageSizeOptions']//span[@data-valmsg-for='PageSizeOptions']//..//input[@id='PageSizeOptions']").send_keys("6,8,9")
 
 
+    # CREATED BY RUPALI
+    def i_should_see_export_to_xml(self):
+        export_to_xml_xpath = "//a[normalize-space()='Export to XML']"
+        export_to_xml = self.driver.find_element(By.XPATH, export_to_xml_xpath).is_displayed()
+        if export_to_xml:
+            assert True
+        else:
+            assert False
+
+    # CREATED BY RUPALI
+    def i_should_see_export_to_excel(self):
+        export_to_excel_xpath = "//a[normalize-space()='Export to Excel']"
+        export_to_excel = self.driver.find_element(By.XPATH, export_to_excel_xpath).is_displayed()
+        if export_to_excel:
+            assert True
+        else:
+            assert False
+
+    # CREATED BY RUPALI
+    def i_should_able_to_select_import_file(self, path):
+        self.driver.find_element(By.XPATH,
+                                 "//div[@class='modal-dialog']//form//input[@id='importexcelfile']").send_keys(
+            f'{path}')
+        time.sleep(2)
+
+    # CREATED BY RUPALI
+    def i_should_able_to_click_button_of_Import_from_excel(self, value):
+        self.driver.find_element(By.XPATH, f"//div[@class='modal-footer']//button[contains(.,'{value}')]").click()
+
+    # CREATED BY RUPALI
+    def i_should_able_to_see_error_message_of_Import(self, childitem):
+        ErrorMessage_1 = '×\nFor security purposes, the feature you have requested is not available on the demo site.'
+        ErrorMessage_2 = '×\nPlease upload a file'
+        ErrorMessage_3 = '×\nSequence contains no elements'
+        SuccessfulMessage = f'×\n{childitem} have been imported successfully.'
+
+        Error_POPUP_text = "//div[@class='alert alert-danger alert-dismissable']"
+        Success_popup_text = "//div[@class='alert alert-success alert-dismissable']"
+
+        if 'alert alert-danger alert-dismissable' in self.driver.page_source:
+            get_error_value = self.driver.find_element(By.XPATH, Error_POPUP_text)
+            item_error_text = get_error_value.text
+
+            if item_error_text == ErrorMessage_1:
+                assert True
+            elif item_error_text == ErrorMessage_2:
+                assert True
+            elif item_error_text == ErrorMessage_3:
+                assert True
+            else:
+                assert False
+        else:
+            get_success_value = self.driver.find_element(By.XPATH, Success_popup_text)
+            item_success_text = get_success_value.text
+
+            if item_success_text == SuccessfulMessage:
+                assert True
+            else:
+                assert False
+
+    # CREATED BY RUPALI
+    def i_should_able_to_enter_category_name_in_Search(self, name):
+        self.driver.find_element(By.XPATH, "//input[@id='SearchCategoryName']").send_keys(f'{name}')
+
+    # CREATED BY RUPALI
+    def i_should_see_published_field_and_select_value(self, publish):
+        if publish == 'All':
+            self.driver.find_element(By.XPATH, "//button[@id='search-categories']").click()
+        elif publish == 'Published only':
+            select = Select(self.driver.find_element(By.XPATH, "//select[@id='SearchPublishedId']"))
+            select.select_by_visible_text(publish)
+        elif publish == 'Unpublished only':
+            select = Select(self.driver.find_element(By.XPATH, "//select[@id='SearchPublishedId']"))
+            select.select_by_visible_text(publish)
+
+    # CREATED BY RUPALI
+    def i_see_search_result(self):
+        # row_data = self.driver.find_elements(By.XPATH, "//div[@id='categories-grid_wrapper']//div//table//tbody//tr")
+
+        if 'No data available in table' in self.driver.page_source:
+            print("no data in table")
+        else:
+            no_of_row = self.driver.find_elements(By.XPATH,
+                                                  "//div[@id='categories-grid_wrapper']//div//table//tbody//tr")
+            length = len(no_of_row)
+            print(length)
+
+    # CREATED BY RUPALI
+    def i_should_click_on_Export_to_XML(self, export):
+        export_to_xpath = f"//a[normalize-space()='{export}']"
+        self.driver.find_element(By.XPATH, export_to_xpath).click()
+
+    # CREATED BY RUPALI
+    def i_should_verify_download_file(self):
+        while not os.path.exists(r'C:\Users\rupal\Downloads'):
+            time.sleep(2)
+        # check file
+            if os.path.isfile(r'C:\Users\rupal\Downloads\categories.xlsx'):
+                print("File download is completed")
+            else:
+                print("File download is not completed")
+        os.remove(r'C:\Users\rupal\Downloads\categories.xlsx')
+
+    # CREATED BY RUPALI
+    def i_should_see_category_table(self):
+        category_table_xpath = "//div[@id='categories-grid_wrapper']//div[@class='row']"
+        table_displayed = self.driver.find_element(By.XPATH, category_table_xpath).is_displayed()
+        if table_displayed:
+            assert True
+        else:
+            assert False
+
+    # CREATED BY RUPALI
+    def i_should_edit_by_category(self, categoryfullname, clickedit):
+        categoryname = "//div[@id='categories-grid_wrapper']//div//table//tbody//tr//td[5]"
+        get_categoryname = self.driver.find_elements(By.XPATH, f"{categoryname}")
+        names_category = []
+        for n in get_categoryname:
+            name_text = n.text
+            names_category.append(name_text)
+        edit_button = f"//div[@id='categories-grid_wrapper']//div//table//tbody//tr[contains(., '{categoryfullname}')]//td[contains(., '{clickedit}')]"
+        while True:
+            time.sleep(3)
+            try:
+                self.driver.find_element(By.XPATH, f"{edit_button}").click()
+                break
+            except Exception:
+                self.driver.find_element(By.XPATH,
+                                         "//div[@id='categories-grid_paginate']//li[@id='categories-grid_next']//a").click()
+            print("Product not in database which you entered")
+
+    # CREATED BY RUPALI
+    def verify_success_save_category(self):
+        success_text_1 = "The category has been updated successfully."
+        success_text_2 = "The new category has been added successfully."
+        Success_popup_text_xpath = "//div[@class='alert alert-success alert-dismissable']"
+        get_success_Value = self.driver.find_element(By.XPATH, Success_popup_text_xpath)
+        item_success_text = get_success_Value.text
+        if 'alert alert-success alert-dismissable' in self.driver.page_source:
+            assert True
+            print(success_text_1)
+        elif item_success_text == success_text_2:
+            assert True
+            print(success_text_2)
+        else:
+            assert False
+
+    # CREATED BY RUPALI
+    def i_should_able_to_click_save(self, btn):
+        if btn == 'Save':
+            self.driver.find_element(By.XPATH, f"//button[normalize-space()='{btn}']").click()
+        elif btn == 'Save and Continue Edit':
+            self.driver.find_element(By.XPATH, f"//button[normalize-space()='{btn}']").click()
+
+    # CREATED BY RUPALI
+    def i_should_able_to_enter_category_name_as(self, field):
+        name_xpath = "//label[normalize-space()='Name']"
+        name_display = self.driver.find_element(By.XPATH, name_xpath).is_displayed()
+        if name_display:
+            self.driver.find_element(By.XPATH, "//input[@id='Name']").clear()
+            self.driver.find_element(By.XPATH, "//input[@id='Name']").send_keys(f'{field}')
+        else:
+            self.driver.find_element(By.XPATH, "//div[contains(text(),'Category info')]").click()
+            if name_display:
+                self.driver.find_element(By.XPATH, "//input[@id='Name']").clear()
+                self.driver.find_element(By.XPATH, "//input[@id='Name']").send_keys(f'{field}')
+
+    # CREATED BY RUPALI
+    def i_should_able_to_select_parent_category_as(self, parent):
+        parent_category_xpath = "//select[@id='ParentCategoryId']"
+        select = Select(self.driver.find_element(By.XPATH, parent_category_xpath))
+        select.select_by_visible_text(parent)
+
+    # CREATED BY RUPALI
+    def i_should_able_to_select_checkbox_under_category(self, checkbox, value):
+        checkbox_xpath = f"//label[normalize-space()='{checkbox}']"
+        checkbox_displayed = self.driver.find_element(By.XPATH, checkbox_xpath).is_displayed()
+        if checkbox_displayed:
+            if value == 'Published':
+                self.driver.find_element(By.XPATH, f"//input[@id='{value}']").click()
+            elif value == 'IncludeInTopMenu':
+                self.driver.find_element(By.XPATH, f"//input[@id='{value}']").click()
+            elif value == 'PriceRangeFiltering':
+                self.driver.find_element(By.XPATH, f"//input[@id='{value}']").click()
--- features/Catalog_Category.feature
+++ features/Catalog_Category.feature
@@ -13,6 +13,14 @@ Feature: Catalog >> Category
     And I click on add new button
     Then I should see "back to category list"
 
+
+  @CC_02 @Rupali
+  Scenario: Verify user must be able to see and click on "Export" button
+    Given I should see "Export" button
+    And I click On Export button
+    Then I should see Export to XML
+    Then I should see Export to Excel
+
   @CC_03
   Scenario: Verify user must be able to see and click on "Import" button
     Given Verify user must be able to see "Import" Button
@@ -21,6 +29,13 @@ Feature: Catalog >> Category
     And Verify user must be able to select Import file path "D:\data\\products (1).xlsx"
     And Verify user must be able to click button of "Import from Excel"
     And Verify user must be able to see the Error Message of "Categories" POP UP
+
+    @CC_04 @Rupali
+  Scenario: Verify user must be able to see and click on "Delete(Selected)" button
+    Given I should see Delete(Selected) button
+    And I click on delete(selected) button
+    And I wait 3 seconds
+    Then I should see "Are you sure?"
 
   @CC_05
   Scenario: Verify user must be able to add new category
@@ -32,11 +47,31 @@ Feature: Catalog >> Category
     Then Click "save"
     Then I should see saved successfully message
 
+
+  @CC_06 @Rupali
+  Scenario: Verify user must be able to download category list as "Export To XML"
+    Given I should see "Export" button
+    And I click On Export button
+    Then I should see Export to XML
+    Then I should able to click on Export to XML Export to XML
+    And I wait 10 seconds
+    Then I should see downloaded file
+
   @CC_07
   Scenario: Verify user must be able to see export option under export button
     Given Verify user must be able to see "Export" Button
     And Verify user must be able to click on drop down options button and select "Export to Excel"
     And Verify user must be able to check downloaded Export file from "C:\Users\User\Downloads\categories.xlsx"
+
+    @CC_08 @Rupali
+  Scenario: Verify user must be able to import category
+    Given I should see Import button
+    Then I click on import button
+    Then I should see choose file popup box
+    Then I should able to select import file path "C:\Users\rupal\Downloads\categories.xlsx"
+    Then I should able to click button of "Import from Excel"
+    And I should see message of "Categories" POP UP
+
 
   @CC_09
   Scenario: Verify user must be able to delete selected category
@@ -45,12 +80,29 @@ Feature: Catalog >> Category
     And Verify user must be able to click "Delete (selected)" Button by name
     And Verify user must be able to click YES on Delete POPUP
 
+    @CC_10 @Rupali
+  Scenario: Verify user must be able to search category with name via All
+    Given I should see "SearchCategoryName" input field by id
+    Then I should able to enter category name "Electronics" in search
+    Then I should see Published field and select value "Published only"
+    Then I should click "Search"
+    Then I see search result by Published field and count number of items
+
+
   @CC_11
   Scenario: Verify user must be able to search category with name via Published only
     Given I should see search "SearchPublishedId" select field by "id"
     And I should click "SearchPublishedId" select field by "id" and select "Published only"
     Then I should click "Search"
     And I see search result by Published Only and Verify
+
+    @CC_12 @Rupali
+  Scenario: Verify user must be able to search category with name via Unpublished only
+    Given I should see "SearchCategoryName" input field by id
+    Then I should able to enter category name "Electronics" in search
+    Then I should see Published field and select value "Unpublished only"
+    Then I should click "Search"
+    Then I see search result by Published field and count number of items
 
   @CC_13
   Scenario: Verify user must be able to edit any category from table
@@ -61,6 +113,18 @@ Feature: Catalog >> Category
     Then Click "save"
     Then I should see saved successfully message
 
+
+  @CC_14 @Rupali
+  Scenario: Verify user must be able to change category name under category info
+    Given I should see see category table
+    Then I should edit "Jewelry" details by click "Edit" under category
+    And I wait 5 seconds
+    Then I should see "Name" input field by id
+    Then I should able to enter category name as test 123
+    Then I should able to click save Save Button
+    And I wait 10 seconds
+    Then I should see saved successfully message on category
+
   @CC_15
   Scenario: Verify user must be able to change category description under category info
     Given I should see data table
@@ -69,6 +133,16 @@ Feature: Catalog >> Category
     And I set description "fdsfds sadasdasdas TEST"
     Then Click "save"
     Then I should see saved successfully message
+
+    @CC_16 @Rupali
+  Scenario: Verify user must be able to change category parent category under category info
+    Given I should see see category table
+    Then I should edit "Jewelry" details by click "Edit" under category
+    And I wait 5 seconds
+    Then I should see "ParentCategoryId" select field by id
+    Then I should able to select Parent category as Apparel >> Shoes
+    Then I should able to click save Save Button
+    Then I should see saved successfully message on category
 
   @CC_17
   Scenario: Verify user must be able to change category picture under category info
@@ -79,6 +153,15 @@ Feature: Catalog >> Category
     Then Click "save"
     Then I should see saved successfully message
 
+    @CC_18 @Rupali
+  Scenario: Verify user must be able to change category published status checkbox as true or false under display
+    Given I should see see category table
+    Then I should edit "Jewelry" details by click "Edit" under category
+    And I wait 5 seconds
+    Then I should able to see and click on checkbox of Published by id Published
+    Then I should able to click save Save Button
+    Then I should see saved successfully message on category
+
   @CC_19
   Scenario: Verify user must be able to change category show on home page checkbox as true or false under display
     Given I should see data table
@@ -88,6 +171,15 @@ Feature: Catalog >> Category
     Then Click "save"
     Then I should see saved successfully message
 
+    @CC_20 @Rupali
+  Scenario: Verify user must be able to change category include top in menu checkbox as true or false under display
+    Given I should see see category table
+    Then I should edit "Jewelry" details by click "Edit" under category
+    And I wait 5 seconds
+    Then I should able to see and click on checkbox of Include in top menu by id IncludeInTopMenu
+    Then I should able to click save Save Button
+    Then I should see saved successfully message on category
+
   @CC_21
   Scenario: Verify user must be able to change category allow customer to select page size checkbox as true or false under display
     Given I should see data table
@@ -96,6 +188,16 @@ Feature: Catalog >> Category
     And Click checkbox of "Allow customers to select page size"
     Then Click "save"
     Then I should see saved successfully message
+
+
+  @CC_22 @Rupali
+  Scenario: Verify user must be able to change category price range filter checkbox as true or false under display
+    Given I should see see category table
+    Then I should edit "Jewelry" details by click "Edit" under category
+    And I wait 5 seconds
+    Then I should able to see and click on checkbox of Price range filtering by id PriceRangeFiltering
+    Then I should able to click save Save Button
+    Then I should see saved successfully message on category
 
   @CC_23
   Scenario: Verify user must be able to change category display order under display
